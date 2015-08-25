38fe2f62786e4f488b53b5b2a7260a1dae12b76b
Avitella <urtashev@gmail.com>
2015-08-25 23:41:36 +0000

    Change filt status line

--- geo_txt_filt.cpp
+++ geo_txt_filt.cpp
@@ -73,8 +73,11 @@ static output_t &operator << (output_t &out, worker_t const &w)
 static output_t &operator << (output_t &out, vector_t<worker_t> const &workers)
 {
 	for (worker_t const &w : workers)
-		if (w.offset != w.points_offset + w.points_count)
+		if (w.offset != w.points_offset + w.points_count) {
 			out << w << " ";
+		} else {
+			out << "\b\b\b\b\b\b\b\b";
+		}
 	return out;
 }
 
