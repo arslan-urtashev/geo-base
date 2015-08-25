7fe105eac447592115cf9f532c4a5ebebcf8facf
Avitella <urtashev@gmail.com>
2015-08-25 12:04:37 +0000

    Added status output

--- tools/geo_base_filt.cpp
+++ tools/geo_base_filt.cpp
@@ -2,6 +2,9 @@
 #include "unordered_set.h"
 #include "log.h"
 
+#include <atomic>
+#include <cassert>
+#include <iomanip>
 #include <iostream>
 #include <random>
 #include <thread>
@@ -13,8 +16,9 @@ static uint32_t const LOOKUP_COUNT = 4000000000ULL;
 
 struct worker_t {
 	geo_base_t const *geo_base;
-	std::mt19937 generator;
 	unordered_set_t<region_id_t> regions;
+	std::mt19937 generator;
+	std::atomic<uint32_t> counter;
 
 	double get_rand()
 	{
@@ -34,12 +38,22 @@ struct worker_t {
 	worker_t(geo_base_t const *g = nullptr, uint32_t seed = 0)
 		: geo_base(g)
 		, generator(seed)
+		, counter(0)
 	{
+	}
+
+	worker_t& operator = (worker_t const &w)
+	{
+		geo_base = w.geo_base;
+		regions = w.regions;
+		generator = w.generator;
+		counter = 0;
+		return *this;
 	}
 
 	void operator () ()
 	{
-		for (size_t i = 0; i < LOOKUP_COUNT; ++i) {
+		for (counter = 0; counter < LOOKUP_COUNT; ++counter) {
 			location_t l(get_rand_lon(), get_rand_lat());
 			region_id_t region_id = geo_base->lookup(l);
 			regions.insert(region_id);
@@ -47,9 +61,23 @@ struct worker_t {
 	}
 };
 
+static output_t &operator << (output_t &out, worker_t const &w)
+{
+	out << w.counter * 100.0 / LOOKUP_COUNT << "%";
+	return out;
+}
+
+static output_t &operator << (output_t &out, vector_t<worker_t> const &w)
+{
+	for (worker_t const &worker : w)
+		out << worker << " ";
+	return out;
+}
+
 int main(int argc, char *argv[])
 {
 	std::ios_base::sync_with_stdio(false);
+	std::cerr << std::fixed << std::setprecision(2);
 
 	if (argc < 2) {
 		log_error("geo-base-filt") << "geo-base-filt <geodata.dat>";
@@ -59,15 +87,28 @@ int main(int argc, char *argv[])
 	try {
 		geo_base_t geo_base(argv[1]);
 
-		std::mt19937 generator;
-
 		vector_t<worker_t> workers(THREADS_COUNT);
-		for (size_t i = 0; i < workers.size(); ++i)
-			workers[i] = worker_t(&geo_base, generator());
+		for (size_t i = 0; i < THREADS_COUNT; ++i)
+			workers[i] = worker_t(&geo_base, i);
 
 		vector_t<std::thread> threads(THREADS_COUNT);
 		for (size_t i = 0; i < threads.size(); ++i)
 			threads[i] = std::thread(std::ref(workers[i]));
+
+		while (true) {
+			bool completed = true;
+
+			for (size_t i = 0; i < workers.size(); ++i)
+				if (workers[i].counter + 1 != LOOKUP_COUNT)
+					completed = false;
+
+			log_info("geo-base-filt", "status") << workers;
+
+			if (completed)
+				break;
+
+			std::this_thread::sleep_for(std::chrono::seconds(1));
+		}
 
 		for (size_t i = 0; i < threads.size(); ++i)
 			threads[i].join();
