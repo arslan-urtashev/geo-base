b39d4c8c2db05dc4b868c2b675d8869ed6dc7029
Avitella <urtashev@gmail.com>
2015-08-26 20:07:14 +0000

    Added geo-base-run name output

--- geo_base_run.cpp
+++ geo_base_run.cpp
@@ -14,6 +14,17 @@ static input_t &operator >> (input_t &in, location_t &l)
 	return in;
 }
 
+static char const *get(char const *n1, char const *n2, char const *n3)
+{
+	if (n1)
+		return n1;
+	if (n2)
+		return n2;
+	if (n3)
+		return n3;
+	return "-- unknown --";
+}
+
 int main(int argc, char *argv[])
 {
 	std::ios_base::sync_with_stdio(false);
@@ -31,10 +42,24 @@ int main(int argc, char *argv[])
 			region_id_t region_id = geo_base.lookup(location);
 
 			if (region_id != UNKNOWN_REGION_ID) {
-				std::cout << region_id << '\n';
+				char const *n1 = nullptr;
+				char const *n2 = nullptr;
+				char const *n3 = nullptr;
 				geo_base.kv_each(region_id, [&] (char const *k, char const *v) {
-					std::cout << "  " << k << " = " << v << '\n';
+					log_debug("geo-base-run", "kv_each") << '"' << k << '"' << ' ' << '"' << v << '"';
+					if (!strcmp(k, "name")) {
+						n1 = v;
+					} else if (!strcmp(k, "name:en")) {
+						n2 = v;
+					} else if (strstr(k, "name") && !n3) {
+						n3 = v;
+					}
 				});
+				std::cout << region_id << " ";
+				std::cout << get(n1, n2, n3);
+				if (n2)
+					std::cout << " (" << n2 << ")";
+				std::cout << '\n';
 			} else {
 				std::cout << -1 << '\n';
 			}
