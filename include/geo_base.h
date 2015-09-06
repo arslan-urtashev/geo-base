42bf26aa9b70c51d064077c1554a59150574a201
avitella <avitella@yandex-team.ru>
2015-09-06 18:49:19 +0300

    visibility fixes

--- include/geo_base.h
+++ include/geo_base.h
@@ -4,8 +4,6 @@
 
 #ifdef __cplusplus
 extern "C" {
-#else
-#	pragma GCC visibility push(default)
 #endif
 
 typedef uint64_t geo_region_id_t;
@@ -28,6 +26,4 @@ geo_region_id_t geo_base_lookup(geo_base_t const *geo_base, geo_location_t const
 
 #ifdef __cplusplus
 }
-#else
-#	pragma GCC visibility pop
 #endif
