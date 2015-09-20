5e33631362310f0759478cd6dd3ca09e3772de41
avitella <avitella@yandex-team.ru>
2015-09-20 23:04:59 +0300

    Added part contains test

--- test/ut/part.cc
+++ test/ut/part.cc
@@ -14,3 +14,21 @@ TEST_F(PartTest, TwoEdges) {
   EXPECT_EQ(0, part.coordinate);
   EXPECT_TRUE(part.Contains(Point(1, 1), geo_data()));
 }
+
+TEST_F(PartTest, PartContainsPoint) {
+  PartHelper part = MakePart(0, {
+    MakeEdge(Point(0, 0), Point(3, 0)),
+    MakeEdge(Point(0, 3), Point(3, 0)),
+    MakeEdge(Point(-1, 10), Point(10, 10)),
+    MakeEdge(Point(-1, 10), Point(10, 100))
+  });
+
+  EXPECT_TRUE(part.Contains(Point(1, 1), geo_data()));
+  EXPECT_TRUE(part.Contains(Point(3, 0), geo_data()));
+  EXPECT_TRUE(part.Contains(Point(0, 11), geo_data()));
+  EXPECT_TRUE(part.Contains(Point(0, 3), geo_data()));
+  
+  EXPECT_FALSE(part.Contains(Point(2, 5), geo_data()));
+  EXPECT_FALSE(part.Contains(Point(2, -1), geo_data()));
+  EXPECT_FALSE(part.Contains(Point(2, 101), geo_data()));
+}
