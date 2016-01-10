39904afd87df6ffa33d54b61e1e90296847d7f54
avitella <avitella@yandex-team.ru>
2016-01-11 02:57:37 +0300

    Added simple objects counter in open_street_map data

---
+++ src/geo_base/open_street_map/simple_counter.h
@@ -0,0 +1,87 @@
+// Copyright (c) 2016 Urtashev Arslan. All rights reserved.
+// Contacts: <urtashev@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+// and associated documentation files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+//   The above copyright notice and this permission notice shall be included in all copies or
+//   substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+// BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#pragma once
+
+#include <geo_base/open_street_map/parser.h>
+
+namespace geo_base {
+namespace open_street_map {
+
+class simple_counter_t : public parser_t {
+public:
+	simple_counter_t(allocator_t *allocator)
+		: parser_t(allocator)
+		, nodes_count_(0)
+		, ways_count_(0)
+		, relations_count_(0)
+	{
+	}
+
+	simple_counter_t(simple_counter_t &&c)
+		: parser_t(std::forward<parser_t>(c))
+		, nodes_count_(0)
+		, ways_count_(0)
+		, relations_count_(0)
+	{
+		std::swap(nodes_count_, c.nodes_count_);
+		std::swap(ways_count_, c.ways_count_);
+		std::swap(relations_count_, c.relations_count_);
+	}
+
+	void process_node(geo_id_t, location_t const &, dynarray_t<kv_t> const &) override
+	{
+		++nodes_count_;
+	}
+
+	void process_way(geo_id_t, dynarray_t<kv_t> const &, dynarray_t<geo_id_t> const &) override
+	{
+		++ways_count_;
+	}
+
+	void process_relation(geo_id_t, dynarray_t<kv_t> const &, dynarray_t<reference_t> const &) override
+	{
+		++relations_count_;
+	}
+
+	size_t nodes_count() const
+	{
+		return nodes_count_;
+	}
+
+	size_t ways_count() const
+	{
+		return ways_count_;
+	}
+
+	size_t relations_count() const
+	{
+		return relations_count_;
+	}
+
+private:
+	size_t nodes_count_;
+	size_t ways_count_;
+	size_t relations_count_;
+
+	simple_counter_t(simple_counter_t const &) = delete;
+	simple_counter_t &operator = (simple_counter_t const) = delete;
+};
+
+} // namespace open_street_map
+} // namespace geo_base
--- test/osm_parser.cc
+++ test/osm_parser.cc
@@ -22,6 +22,7 @@
 #include <geo_base/open_street_map/open_street_map.h>
 #include <geo_base/open_street_map/parser.h>
 #include <geo_base/open_street_map/reader.h>
+#include <geo_base/open_street_map/simple_counter.h>
 #include <geo_base/util/fd_guard.h>
 #include <geo_base/util/file.h>
 #include <geo_base/util/file_stream.h>
@@ -32,52 +33,6 @@
 using namespace geo_base;
 using namespace open_street_map;
 
-struct counter_t : public parser_t {
-	explicit counter_t(allocator_t *allocator)
-		: parser_t(allocator)
-		, nodes_amount(0)
-		, ways_amount(0)
-		, relations_amount(0)
-	{
-	}
-
-	counter_t(counter_t &&c)
-		: parser_t(std::forward<parser_t>(c))
-		, nodes_amount(0)
-		, ways_amount(0)
-		, relations_amount(0)
-	{
-		std::swap(nodes_amount, c.nodes_amount);
-		std::swap(ways_amount, c.ways_amount);
-		std::swap(relations_amount, c.relations_amount);
-	}
-
-	void process_node(geo_id_t, location_t const &,
-		dynarray_t<kv_t> const &) override
-	{
-		++nodes_amount;
-	}
-
-	void process_way(geo_id_t, dynarray_t<kv_t> const &,
-		dynarray_t<geo_id_t> const &) override
-	{
-		++ways_amount;
-	}
-
-	void process_relation(geo_id_t, dynarray_t<kv_t> const &,
-		dynarray_t<reference_t> const &) override
-	{
-		++relations_amount;
-	}
-
-	size_t nodes_amount;
-	size_t ways_amount;
-	size_t relations_amount;
-
-	counter_t(counter_t const &) = delete;
-	counter_t &operator = (counter_t const) = delete;
-};
-
 TEST(osm_parser, osm_parser)
 {
 	pool_allocator_t allocator(1_mb);
@@ -86,35 +41,35 @@ TEST(osm_parser, osm_parser)
 	file_input_stream_t input_stream(file.fd());
 
 	reader_t reader(&input_stream);
-	counter_t amount(&allocator);
+	simple_counter_t count(&allocator);
 
-	amount.parse(&reader);
+	count.parse(&reader);
 
-	EXPECT_EQ(123736ULL, amount.nodes_amount);
-	EXPECT_EQ(5750ULL, amount.ways_amount);
-	EXPECT_EQ(142ULL, amount.relations_amount);
+	EXPECT_EQ(123736ULL, count.nodes_count());
+	EXPECT_EQ(5750ULL, count.ways_count());
+	EXPECT_EQ(142ULL, count.relations_count());
 
 	log_info("Parsed %lu relations, %lu ways and %lu nodes",
-		amount.relations_amount, amount.ways_amount, amount.nodes_amount);
+		count.relations_count(), count.ways_count(), count.nodes_count());
 }
 
 TEST(osm_parser, run_pool_parse)
 {
 	std::vector<pool_allocator_t> allocators;
-	std::vector<counter_t> counters;
+	std::vector<simple_counter_t> simple_counters;
 
 	allocators.emplace_back(1_mb);
 	allocators.emplace_back(1_mb);
 
-	counters.emplace_back(&allocators[0]);
-	counters.emplace_back(&allocators[1]);
+	simple_counters.emplace_back(&allocators[0]);
+	simple_counters.emplace_back(&allocators[1]);
 
-	run_pool_parse("test/osm/andorra-latest.osm.pbf", counters);
+	run_pool_parse("test/osm/andorra-latest.osm.pbf", simple_counters);
 
-	EXPECT_EQ(123736ULL, counters[0].nodes_amount + counters[1].nodes_amount);
-	EXPECT_EQ(5750ULL, counters[0].ways_amount + counters[1].ways_amount);
-	EXPECT_EQ(142ULL, counters[0].relations_amount + counters[1].relations_amount);
+	EXPECT_EQ(123736ULL, simple_counters[0].nodes_count() + simple_counters[1].nodes_count());
+	EXPECT_EQ(5750ULL, simple_counters[0].ways_count() + simple_counters[1].ways_count());
+	EXPECT_EQ(142ULL, simple_counters[0].relations_count() + simple_counters[1].relations_count());
 
-	log_info("Blocks processed 1 = %lu", counters[0].blocks_processed());
-	log_info("Blobks processed 2 = %lu", counters[1].blocks_processed());
+	log_info("Blocks processed 1 = %lu", simple_counters[0].blocks_processed());
+	log_info("Blobks processed 2 = %lu", simple_counters[1].blocks_processed());
 }
