53231cf9e4fed823e73cfce861d45b1e9e4d3c62
avitella <avitella@yandex-team.ru>
2016-01-13 04:57:37 +0300

    Free google protobuf in unit tests

--- test/run_ut.cc
+++ test/run_ut.cc
@@ -1,6 +1,33 @@
-#include <gmock/gmock.h>
+// Copyright (c) 2016 Urtashev Arslan. All rights reserved.
+// Contacts: <urtashev@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+// and associated documentation files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+//   The above copyright notice and this permission notice shall be included in all copies or
+//   substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+// BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-TEST(hello, hello)
+#include <gmock/gmock.h>
+#include <google/protobuf/stubs/common.h>
+#include <gtest/gtest.h>
+
+int main(int argc, char *argv[])
 {
-	ASSERT_EQ("Hello!", "Hello!");
+	std::cout << "Running unit tests" << std::endl;
+
+	testing::InitGoogleMock(&argc, argv);
+	int const ret = RUN_ALL_TESTS();
+
+	google::protobuf::ShutdownProtobufLibrary();
+
+	return ret;
 }
