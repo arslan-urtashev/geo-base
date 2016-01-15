02413d64b220450a31c2c15ae03f36579b57857d
Avitella <urtashev@gmail.com>
2016-01-15 20:22:49 +0300

    Fix build

--- src/geo_base/open_street_map/converter.cc
+++ src/geo_base/open_street_map/converter.cc
@@ -16,6 +16,7 @@
 // DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+#include <arpa/inet.h>
 #include <geo_base/open_street_map/converter.h>
 #include <geo_base/polygon.h>
 #include <geo_base/region.h>
