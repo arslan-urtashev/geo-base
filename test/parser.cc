f4da1f5da9a4b272b4cc43370f865b7ac08fec19
avitella <avitella@yandex-team.ru>
2016-01-15 18:45:10 +0300

    Added double parse test

--- test/parser.cc
+++ test/parser.cc
@@ -76,3 +76,133 @@ TEST(open_street_map_parser, run_pool_parse)
 	log_info("Blocks processed 1 = %lu", simple_counters[0].blocks_processed());
 	log_info("Blobks processed 2 = %lu", simple_counters[1].blocks_processed());
 }
+
+class hash_counter_t : public parser_t {
+public:
+	static uint64_t const POWER = 719ull;
+
+	hash_counter_t(allocator_t *allocator)
+		: parser_t(allocator)
+		, hash_(0)
+		, power_(1)
+	{
+	}
+
+	static uint64_t kvs_hash(kvs_t const &kvs)
+	{
+		static uint64_t KVS_POWER = 117ull;
+		uint64_t hash = 0, power = 1;
+		for (kv_t const &kv : kvs) {
+			for (char const *c = kv.k; *c; ++c) {
+				hash += ((uint64_t) *c) * power;
+				power *= KVS_POWER;
+			}
+			for (char const *c = kv.v; *c; ++c) {
+				hash += ((uint64_t) *c) * power;
+				power *= KVS_POWER;
+			}
+		}
+		return power;
+	}
+
+	static uint64_t ids_hash(geo_ids_t const &ids)
+	{
+		static uint64_t IDS_POWER = 317ull;
+		uint64_t hash = 0, power = 1;
+		for (geo_id_t const &id : ids) {
+			hash += id * power;
+			power *= IDS_POWER;
+		}
+		return hash;
+	}
+
+	static uint64_t refs_hash(references_t const &refs)
+	{
+		static uint64_t REF_POWER = 319ull;
+		uint64_t hash = 0, power = 1;
+		for (reference_t const &ref : refs) {
+			hash += ref.geo_id * power;
+			power *= REF_POWER;
+			hash += ref.type * power;
+			power *= REF_POWER;
+			for (char const *c = ref.role; *c; ++c) {
+				hash += ((uint64_t) *c) * power;
+				power *= REF_POWER;
+			}
+		}
+		return hash;
+	}
+
+	void process_node(geo_id_t geo_id, location_t const &location, kvs_t const &kvs) override
+	{
+		uint64_t node_hash = 0;
+		node_hash += geo_id * 777ull + location.lat * 333ull + location.lon * 123ull;
+		node_hash += kvs_hash(kvs) * 765ull;
+		hash_ += node_hash * power_;
+		power_ *= POWER;
+	}
+
+	void process_way(geo_id_t geo_id, kvs_t const &kvs, geo_ids_t const &ids) override
+	{
+		uint64_t way_hash = geo_id * 723ull + kvs_hash(kvs) * 111ull + ids_hash(ids) * 567ull;
+		hash_ += way_hash * power_;
+		power_ *= POWER;
+	}
+
+	void process_relation(geo_id_t geo_id, kvs_t const &kvs, references_t const &refs) override
+	{
+		uint64_t relation_hash = geo_id * 237ull + kvs_hash(kvs) * 222ull + refs_hash(refs) * 999ull;
+		hash_ += relation_hash * power_;
+		power_ *= POWER;
+	}
+
+	uint64_t hash() const
+	{
+		return hash_;
+	}
+
+	uint64_t power() const
+	{
+		return power_;
+	}
+
+private:
+	uint64_t hash_;
+	uint64_t power_;
+};
+
+TEST(open_street_map_parser, double_parse)
+{
+	pool_allocator_t allocator(1_mb);
+	uint64_t hash = 0, power = 0;
+
+	{
+		file_t file;
+		file.read_open(OSM_PBF_TEST_FILE);
+		file_input_stream_t input_stream(file.fd());
+
+		reader_t reader(&input_stream);
+		hash_counter_t counter(&allocator);
+
+		counter.parse(&reader);
+		
+		hash = counter.hash();
+		power = counter.power();
+	}
+
+	{
+		file_t file;
+		file.read_open(OSM_PBF_TEST_FILE);
+		file_input_stream_t input_stream(file.fd());
+
+		reader_t reader(&input_stream);
+		hash_counter_t counter(&allocator);
+
+		counter.parse(&reader);
+
+		ASSERT_EQ(hash, counter.hash());
+		ASSERT_EQ(power, counter.power());
+	}
+}
+
+
