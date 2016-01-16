7ca979ecb7afe95a653551d3807068193de0a9bf
avitella <avitella@yandex-team.ru>
2016-01-16 03:12:35 +0300

    Fix build

--- geo_base/proto_util/proto_reader.h
+++ geo_base/proto_util/proto_reader.h
@@ -16,6 +16,7 @@
 // DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+#include <arpa/inet.h>
 #include <geo_base/proto/region.pb.h>
 #include <geo_base/typedef.h>
 #include <geo_base/util/allocator.h>
