63800896a63e9d51c0842603fa852bb3049613be
avitella <avitella@yandex-team.ru>
2016-01-25 00:14:19 +0300

    Attemt to fix travis build

--- .travis.sh
+++ .travis.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
-install_linux_dependencies() {
+function install_linux_dependencies() {
     sudo add-apt-repository ppa:webupd8team/java
     sudo apt-get update
     sudo apt-get install oracle-java8-installer
     sudo apt-get install pkg-config zip g++ zlib1g-dev unzip
 }
 
-install_linux_bazel() {
+function install_linux_bazel() {
     wget https://github.com/bazelbuild/bazel/releases/download/0.1.4/bazel-0.1.4-installer-linux-x86_64.sh
     chmod +x bazel-0.1.4-installer-linux-x86_64.sh
     ./bazel-0.1.4-installer-linux-x86_64.sh --user
     export PATH="$PATH:$HOME/bin"
 }
 
-install_osx_bazel() {
+function install_osx_bazel() {
     wget https://github.com/bazelbuild/bazel/releases/download/0.1.4/bazel-0.1.4-installer-darwin-x86_64.sh
     chmod +x bazel-0.1.4-installer-darwin-x86_64.sh
     ./bazel-0.1.4-installer-darwin-x86_64.sh --user
