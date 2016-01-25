719b9bd72bb84a44a24dae41b5eb03739311fcc8
avitella <avitella@yandex-team.ru>
2016-01-25 22:12:12 +0300

    Attemt to fix osx build

--- .travis.sh
+++ .travis.sh
@@ -14,8 +14,15 @@ function install_osx_bazel() {
     export PATH="$PATH:$HOME/bin"
 }
 
+function install_osx_java8() {
+    wget --no-cookies --no-check-certificate --header \"Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie\" http://download.oracle.com/otn-pub/java/jdk/8u66-b17/jdk-8u66-macosx-x64.dmg
+    hdiutil mount jdk-8u66-macosx-x64.dmg
+    sudo installer -pkg /Volumes/JDK\ 8\ Update\ 66/JDK\ 8\ Update\ 66.pkg -target LocalSystem
+}
+
 if [ $(uname -s) == "Linux" ]; then
     install_linux_bazel
 else
+    install_osx_java8
     install_osx_bazel
 fi
